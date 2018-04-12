4021b9aa2f91c8d0a13707f438cf2bb6d1b8c9d9
Azrul Aziz <azrultres@gmail.com>
2018-04-13 05:54:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Location from './components/Location';
 import Display from './components/Display';
 import Menu from './components/Menu';
-// todo: Transition during location change. 
+
+// arrays of cities 
 const location = {
   area: ['Kuala Lumpur', 'Labuan', 'Putrajaya', 'Shah Alam', 'Pulau Pinang', 'Johor Bahru', 'Kota Bharu', 'Kuching']
 }
 
+// the location code for the cities. not able to retrieve from the API.
 const code = {
   "Kuala Lumpur": 'wlp-0',
   Labuan: 'wlp-1',
@@ -19,6 +21,7 @@ const code = {
   Kuching: 'swk-17',
 }
 
+// setup the url to make the API calls
 const url = `https://mpt.i906.my/mpt.json?`;
 const filter = `&filter=1`;
 
@@ -35,14 +38,15 @@ class App extends Component {
       currLocation: 'Kuala Lumpur',
       toggle: false,
     }
-
+    // bind all functions to make sure the 'this' keyword are correctly point
     this.handleCodeRetrieval = this.handleCodeRetrieval.bind(this);
     this.fetchTime = this.fetchTime.bind(this);
     this.setTime = this.setTime.bind(this);
     this.handleSideMenu = this.handleSideMenu.bind(this);
     this.handleMenu = this.handleMenu.bind(this);
   }
-
+  
+  // the function to retrieve the location code based on user interaction
   handleCodeRetrieval(e) {
     const value = e.target.innerText;
     this.fetchTime(code[value]);
@@ -53,17 +57,20 @@ class App extends Component {
     const {searchTerm} = this.state;
     this.fetchTime(searchTerm);
   }
-
+  
+  // setup the Fetch API to make the api calls after component is mounted
   fetchTime(searchTerm) {
     fetch(`${url}code=${searchTerm}${filter}`)
     .then(response => response.json())
     .then(result => this.setTime(result))
   }
-
+  
+  // set the result retrive from the api inside the local state
   setTime(result) {
     this.setState({resultTimes: result.response.times})
   }
-
+  
+  // the function to handle sidebar transition
   handleSideMenu() {
     this.setState({toggle: !this.state.toggle})
   }
