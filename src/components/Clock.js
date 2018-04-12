d45b0b0a4111c3afa778cbf0f283dfdf1f3f4c41
Azrul Aziz <azrultres@gmail.com>
2018-04-13 05:58:22 +0800

    Update Clock.js

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+// iniate a clock class to handle the real time clock
 class Clock extends Component {
     constructor(props) {
         super(props);
@@ -10,14 +11,17 @@ class Clock extends Component {
         }
     }
     
+    // once the component is mounted, start the interval 
     componentDidMount() {
         this.interval = setInterval(() => this.tick(), 1000);
     }
-
+    
+    // clear the interval when leaving the page
     componentWillUnmount() {
         clearInterval(this.inverval);
     }
-
+    
+    // the function to handle the state changes at every interval
     tick() {
         this.setState({time: moment().format(`MMMM Do YYYY // h:mm:ss A`)})
     }
